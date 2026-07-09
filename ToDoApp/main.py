fcd956982ebad9a04cb6257baa2bcfa8f888d7bb
brianjb-abw <brianjb-tek@proton.me>
2026-07-09 02:52:00 -0400

    added new todo post endpt w pydantic class

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,4 +1,6 @@
 from typing import Annotated
+
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, status, Path
 import models
@@ -17,6 +19,13 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=2, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
 
 
 
@@ -39,3 +48,12 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail=f'Todo id {todo_id} not found.')
+
+
+# ---- post todo
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
